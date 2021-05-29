60b4dc23c58a382ddb118c5f5255bb0214f0fd8e
Albeiro Marin <xlandru@hotmail.com>
2021-05-29 12:53:44 -0500

    first commit

---
+++ src/App.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import mullter from 'multer';
+
+export const app = express();
+
+
+
+
+
---
+++ src/www/index.ts
@@ -0,0 +1,10 @@
+import {app} from '../App';
+import dotenv from 'dotenv';
+
+dotenv.config({path: `${__dirname}/../../.env`});
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`listening on port ${process.env.PORT}`);
+});
