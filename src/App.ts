e91b9a7d9e1801e26ac06ed8bfac9977be5969ef
Albeiro Marin <xlandru@hotmail.com>
2021-06-02 20:11:29 -0500

    Solve Cors conection issues

--- src/App.ts
+++ src/App.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import {router} from './routes/index';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
+
 app.use("/api", router);
+
+
+
 
 
 export default app;
