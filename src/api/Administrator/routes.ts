79ff5981612cb6680aa1c95640b264cb03312b52
Albeiro Marin <xlandru@hotmail.com>
2021-06-07 16:30:16 -0500

    Create Administrator login

---
+++ src/api/Administrator/routes.ts
@@ -0,0 +1,6 @@
+import express from 'express';
+import {getIndividualAdministrator} from "./controller";
+
+export const router =  express.Router();
+
+router.get('/:username/:password', getIndividualAdministrator);
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 import {router as products} from '../api/products/routes';
+import {router as administrator} from '../api/Administrator/routes';
 
 export const router = express.Router();
 
 router.use("/products", products);
+router.use("/administrator", administrator);
+
 
