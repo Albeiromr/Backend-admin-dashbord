3ae54ccb2aae9116ddd6f2fdbcb92b614afa57ce
Albeiro Marin <xlandru@hotmail.com>
2021-06-12 11:15:32 -0500

    Change mysql single connection to a connection pool

--- src/database/database.ts
+++ src/database/database.ts
@@ -2,11 +2,19 @@ import mysql from 'mysql';
 import dotenv from 'dotenv';
 dotenv.config({path: `${__dirname}/../../.env`});
 
-export const connection = mysql.createConnection({
+/* export const connection = mysql.createConnection({
   host     : process.env.DATABASE_HOST,
   user     : process.env.DATABASE_USER,
   password : process.env.DATABASE_PASSWORD,
   database : process.env.DATABASE_DATABASE,
+}); */
+
+export const connection = mysql.createPool({
+  connectionLimit: 20,
+  host     : process.env.DATABASE_HOST,
+  user     : process.env.DATABASE_USER,
+  password : process.env.DATABASE_PASSWORD,
+  database : process.env.DATABASE_DATABASE
 });
 
 
