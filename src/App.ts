224d93740a8e04c4ff1dc92855c769a4194f3624
Albeiro Marin <xlandru@hotmail.com>
2021-06-17 13:47:42 -0500

    Add frontend react production build folder

--- src/App.ts
+++ src/App.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import path from 'path';
 import {router} from './routes/index';
 import cors from 'cors';
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, '/../build')));
 app.use('/static',express.static(`${__dirname}/public/thumbnail`));
 app.use('/static',express.static(`${__dirname}/public/bigImg`));
 app.use(express.json());
@@ -12,6 +14,9 @@ app.use(cors())
 
 app.use("/api", router);
 
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/../build', 'index.html'));
+});
 
 
 
